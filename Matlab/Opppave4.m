e75a887155abaf31939430734d5d9f18e289e011
ericwern <eriwern@gmail.com>
2016-04-14 15:52:16 +0200

    Ferdig med 4 cde (trenger feilsjekk)

--- Matlab/Opppave4.m
+++ Matlab/Opppave4.m
@@ -1,25 +1,57 @@
-disp('Oppgave 4')
+clear;
+clc;
+format longE
 
+
+disp('Oppgave 4c')
 [E, I, D, d, w, f, g, L] = hentKonstanter();
 
-v = zeros(1,10);
+% Regner ut ye for den eksakte løsningen.
+ye = zeros(1,10);
 count = 1;
-
 for i = (0.2:0.2:2)
-    v(count) = correct_y(f,E,I,L,i);
+    ye(count) = correct_y(f,E,I,L,i);
     count = count + 1;
 end
+ye = transpose(ye);
+ye
 
-v
 
-disp('Ganger sammen Ay')
+% Lager samme A som oppgave 2 og regner ut A*ye.
+disp('Regner ut C = Ay (A hentet fra oppgave 2)')
 A = lagA(10);
-B = transpose(v);
+C = A*ye;
+C
 
-C = A*B;
-full(C)
 
-disp('Sammenligner svaret fra oppgave 3')
-b = ebbeam(L,10,f,E,I)
+disp('Oppgave 4d')
+disp('Sammenligner svaret C fra c) med vektoren b fra oppgave 3')
+% Gjenskaper vektoren b fra oppgave 3.
+h = L/10;
+b = repmat(f, 10, 1) * h^4/(E*I)
 
-T = table(B, b)
+table(C, b)
+
+% Finner foroverfeil ved å ta ||C - b||
+disp('foroverfeil:')
+FE = max( abs(C-b) )
+
+% Finner relativ foroverfeil ved å ta ||C - b|| / ||C||
+disp('Relativ foroverfeil:')
+rFE = (FE)/( max( abs(C) ) )
+
+% Antar at relativ bakoverfeil er rBE=2^-52 og regner ut
+% feilforstørrelsen som rFE/rBE
+disp('Feilforstørrelse:')
+rBE = 2^-52;
+rFE/rBE
+condest(A)
+
+disp('Oppgave 4e')
+disp('Sammenligner den eksakte løsningen (ye) med vektoren (yc) fra opgave 3')
+
+yc = ebbeam(L,10,f,E,I);
+table(yc, ye)
+
+disp('foroverfeil: ||yc-ye||1')
+max(abs(yc-ye))
